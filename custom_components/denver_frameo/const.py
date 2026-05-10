f655d49184ed7378a14baf4e4b8589d172979b06
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-10 21:33:55 +0200

    Remove duplicate DOMAIN definition in const.py

--- custom_components/denver_frameo/const.py
+++ custom_components/denver_frameo/const.py
@@ -1,11 +1,7 @@
 DOMAIN = "denver_frameo"
-
 DEFAULT_PORT = 5555
 SCAN_INTERVAL = 5
-
 LOGGER_NAME = "denver_frameo"
-
-DOMAIN = "denver_frameo"
 NAME = "Denver Frameo Photoframe"
 MANUFACTURER = "Denver"
 MODEL = "PFF-1082"
