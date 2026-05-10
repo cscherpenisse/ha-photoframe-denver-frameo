1b7fa4df3d7ae41d12346bcf2919513e6a163fd9
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-10 19:53:00 +0200

    Add FrameoBrightnessSensor for LED brightness

---
+++ custom_components/denver_frameo/sensor.py
@@ -0,0 +1,21 @@
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    coordinator = hass.data["denver_frameo"][entry.entry_id]
+
+    async_add_entities([
+        FrameoBrightnessSensor(coordinator)
+    ])
+
+
+class FrameoBrightnessSensor(CoordinatorEntity, SensorEntity):
+    _attr_name = "Frameo LED Brightness"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+
+    @property
+    def native_value(self):
+        return self.coordinator.data["brightness"]
