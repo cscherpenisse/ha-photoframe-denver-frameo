03f60217e79bfa0a69132b1acb34e15218ec5346
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-10 20:48:46 +0200

    Refactor async_setup_entry and async_unload_entry methods

--- custom_components/denver_frameo/__init__.py
+++ custom_components/denver_frameo/__init__.py
@@ -3,23 +3,27 @@ from homeassistant.core import HomeAssistant
 
 from .coordinator import FrameoCoordinator
 
-PLATFORMS = ["light", "switch", "sensor"]
-
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass, entry):
     coordinator = FrameoCoordinator(hass, entry)
 
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault("denver_frameo", {})[entry.entry_id] = coordinator
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    
+    await hass.config_entries.async_forward_entry_setup(entry, "light")
+    await hass.config_entries.async_forward_entry_setup(entry, "switch")
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+async def async_unload_entry(hass, entry):
+    unload_ok = True
+
+    for platform in ("light", "switch", "sensor"):
+        unload_ok &= await hass.config_entries.async_forward_entry_unload(
+            entry, platform
+        )
 
     if unload_ok:
         hass.data["denver_frameo"].pop(entry.entry_id)
