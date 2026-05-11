37733a358d22af4bda9bffc3bff598fd9213b738
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-11 20:03:18 +0200

    Refactor domain name usage in setup functions

--- custom_components/denver_frameo/__init__.py
+++ custom_components/denver_frameo/__init__.py
@@ -1,27 +1,53 @@
+import importlib
+
 from .coordinator import FrameoCoordinator
 
-PLATFORMS = ["light", "switch", "sensor", "button", "media_player"]
+DOMAIN = "denver_frameo"
+
+PLATFORMS = [
+    "light",
+    "switch",
+    "sensor",
+    "button",
+    "media_player",
+]
 
 
 async def async_setup_entry(hass, entry):
+    """Setup Denver Frameo entry."""
+
+    # Preload platforms to avoid blocking import warnings
+    for platform in PLATFORMS:
+        importlib.import_module(
+            f".{platform}",
+            package=__package__,
+        )
+
     coordinator = FrameoCoordinator(hass, entry)
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault("denver_frameo", {})[entry.entry_id] = coordinator
-    
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    hass.data.setdefault(DOMAIN, {})[
+        entry.entry_id
+    ] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
 
     return True
 
 
 async def async_unload_entry(hass, entry):
+    """Unload Denver Frameo entry."""
+
     unload_ok = await hass.config_entries.async_unload_platforms(
         entry,
         PLATFORMS,
     )
 
     if unload_ok:
-        hass.data["denver_frameo"].pop(entry.entry_id)
+        hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
