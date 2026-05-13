6e56c65e4380852363319e88025dcdae77b175f9
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-13 19:30:08 +0200

    Add FrameoForegroundAppSensor to sensor.py

--- custom_components/denver_frameo/sensor.py
+++ custom_components/denver_frameo/sensor.py
@@ -8,7 +8,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data["denver_frameo"][entry.entry_id]
 
     async_add_entities([
-        FrameoBrightnessSensor(coordinator)
+        FrameoBrightnessSensor(coordinator),
+        FrameoForegroundAppSensor(coordinator),
     ])
 
 
@@ -22,3 +23,25 @@ class FrameoBrightnessSensor(CoordinatorEntity, SensorEntity):
     @property
     def native_value(self):
         return self.coordinator.data["brightness"]
+
+class FrameoForegroundAppSensor(
+    CoordinatorEntity,
+    SensorEntity,
+):
+    _attr_name = "Foreground App"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+
+        self._attr_device_info = (
+            get_device_info(
+                coordinator.config_entry
+            )
+        )
+
+    @property
+    def native_value(self):
+        return self.coordinator.data.get(
+            "foreground_app",
+            "unknown",
+        )
