bd434259a4b9a8886e5e3965c9d91644386dce1b
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-13 18:27:44 +0200

    Implement FrameoScreenBrightness number entity

---
+++ custom_components/denver_frameo/number.py
@@ -0,0 +1,59 @@
+from homeassistant.components.number import (
+    NumberEntity,
+)
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+)
+
+from .device import get_device_info
+
+
+async def async_setup_entry(
+    hass,
+    entry,
+    async_add_entities,
+):
+    coordinator = hass.data["denver_frameo"][
+        entry.entry_id
+    ]
+
+    async_add_entities([
+        FrameoScreenBrightness(coordinator)
+    ])
+
+
+class FrameoScreenBrightness(
+    CoordinatorEntity,
+    NumberEntity,
+):
+    _attr_name = "Screen Brightness"
+
+    _attr_native_min_value = 0
+    _attr_native_max_value = 255
+    _attr_native_step = 1
+
+    _attr_mode = "slider"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+
+        self._attr_device_info = get_device_info(
+            coordinator.config_entry
+        )
+
+    @property
+    def native_value(self):
+        return self.coordinator.data.get(
+            "screen_brightness",
+            127,
+        )
+
+    async def async_set_native_value(
+        self,
+        value,
+    ):
+        await self.coordinator.adb.set_screen_brightness(
+            int(value)
+        )
+
+        await self.coordinator.async_request_refresh()
