8dbc8629776ff8f8cdb0f80b253cd40fdf7b959d
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-15 09:53:48 +0200

    Change state attribute to _attr_state

--- custom_components/denver_frameo/media_player.py
+++ custom_components/denver_frameo/media_player.py
@@ -60,7 +60,7 @@ class FrameoMediaPlayer(
 
         self.coordinator = coordinator
         
-        self._state = MediaPlayerState.OFF
+        self._attr_state = MediaPlayerState.OFF
         
         self._attr_unique_id = (
             f"{coordinator.config_entry.entry_id}_media"
