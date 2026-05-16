368e4baa580f3d9406a1471b195467d7a542536f
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-16 07:00:00 +0200

    Refactor device_info assignment in switch.py

--- custom_components/denver_frameo/switch.py
+++ custom_components/denver_frameo/switch.py
@@ -18,12 +18,12 @@ class FrameoScreenSwitch(CoordinatorEntity, SwitchEntity):
         super().__init__(coordinator)
         self._attr_unique_id = (f"{coordinator.config_entry.entry_id}_frameo_screen")
         ###self._attr_device_info = get_device_info(coordinator.config_entry)
-    self._attr_device_info = (
-                get_device_info(
-                    coordinator.config_entry
+        self._attr_device_info = (
+                    get_device_info(
+                        coordinator.config_entry
+                    )
                 )
-            )
-
+    
     
     @property
     def is_on(self):
