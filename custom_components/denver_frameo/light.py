8c327549e231983d92dba1c2c7cd76d5fffde9d3
Corné Scherpenisse <117529571+cscherpenisse@users.noreply.github.com>
2026-05-16 10:25:24 +0200

    Set unique ID for Frameo LEDs in light component

--- custom_components/denver_frameo/light.py
+++ custom_components/denver_frameo/light.py
@@ -19,10 +19,10 @@ class FrameoLedLight(CoordinatorEntity, LightEntity):
 
     def __init__(self, coordinator):
         super().__init__(coordinator)
-
-        # ✅ HIER hoort het
+        
+        self._attr_unique_id = (f"{coordinator.config_entry.entry_id}_frameo_leds")       
         self._attr_device_info = get_device_info(coordinator.config_entry)
-
+    
     @property
     def is_on(self):
         return self.coordinator.data["is_on"]
